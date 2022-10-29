5199f1a244ebc16552b5becf4162f4e4af69e16c
jstdk <joost@dekruijff.email>
2022-10-29 19:26:16 +0630

    first commit

---
+++ lib/__init__.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+
+def count_in_list(l, word):
+    c = Counter(l)
+    return c[word]
---
+++ run.py
@@ -0,0 +1,50 @@
+from realtime.connection import Socket
+from realtime import Socket
+import asyncio
+import time
+import socket
+import imaplib
+import traceback
+from imap_tools import A, MailBox, MailboxLoginError, MailboxLogoutError
+
+from dotenv import dotenv_values
+from supabase import create_client, Client
+from realtime.connection import Socket
+
+config = dotenv_values(".env")
+url: str = config['SUPABASE_URL']
+key: str = config['SUPABASE_KEY']
+supabase: Client = create_client(url, key)
+
+SUPABASE_ID = config['SUPABASE_ID']
+API_KEY = config['SUPABASE_KEY']
+
+done = False
+while not done:
+    connection_start_time = time.monotonic()
+    connection_live_time = 0.0
+    try:
+        with MailBox('imap.gmail.com').login('joost.de.kruijff@flymya.co', 'xfwvmarwgsdtjdyh', 'INBOX') as mailbox:
+            print('Imap poller started', time.asctime())
+            while connection_live_time < 29 * 60:
+                try:
+                    responses = mailbox.idle.wait(timeout=3 * 60)
+                    print(time.asctime(), 'IDLE responses:', responses)
+                    if responses:
+                        for msg in mailbox.fetch(A(seen=False)):
+                            print('->', msg.date, msg.subject)
+                            data = supabase.table("entries").insert(
+                                {"entry": msg.subject}).execute()
+                            assert len(data.data) > 0
+                            print('New email stored in database')
+                except KeyboardInterrupt:
+                    print('~KeyboardInterrupt')
+                    done = True
+                    break
+                connection_live_time = time.monotonic() - connection_start_time
+    except (TimeoutError, ConnectionError,
+            imaplib.IMAP4.abort, MailboxLoginError, MailboxLogoutError,
+            socket.herror, socket.gaierror, socket.timeout) as e:
+        print(
+            f'## Error\n{e}\n{traceback.format_exc()}\nreconnect in a minute...')
+        time.sleep(60)
